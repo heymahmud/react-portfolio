b47a82c6f692ad75b93c0e3aaf2506ad2f6cb0f1
amir <iamamirmostafa@gmail.com>
2021-07-31 08:17:44 +0600

    toggler update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Home from './components/Home/Home';
 import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
+import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
 
 function App() {
