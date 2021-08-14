125851d94e9fc38d56f509fca12d03aa188e8e8e
amir <iamamirmostafa@gmail.com>
2021-08-15 02:17:13 +0600

    update href

--- src/fakeData/PrevProjects.js
+++ src/fakeData/PrevProjects.js
@@ -1,32 +1,20 @@
 const projects = [
     {
         id: 1,
-        name: "Netflix clone",
-        description: "Netflix clone using React JS, React Movie Trailer, React Youtube with Fully Mobile Responsive",
-        github: "https://github.com/AmirMostofaa/netflix-clone",
-        live: "https://netflix-clone-by-amir.web.app/",
-        img: "https://i.ibb.co/wsgvMHk/netflix-min.jpg",
+        name: "Carserv | Auto Repair",
+        description: "A MERN-Stack Car Auto Repair Webapp where we can add Orders, Services, testimonials, Team members. and We can Manage everything also Team has All the Rules. Customer can Make order and Check the Order List",
+        github: "https://github.com/AmirMostofaa/carserv-client",
+        live: "https://carserv-76.web.app/",
+        img: "https://i.ibb.co/R4TC7r7/carserv.png",
         tech1: "React",
-        tech2: "React Youtube",
-        tech3: "React Movie Trailer",
-        tech4: "Firebase"
+        tech2: "Express JS",
+        tech3: "MongoDB",
+        tech4: "Firebase, Heroku"
     },
-    // {
-    //     id: 1,
-    //     name: "Carserv | Auto Repair",
-    //     description: "A Full-stack Car Auto Repair Webapp where we can add Orders, Services, testimonials, Team members. and We can Manage everything",
-    //     github: "https://github.com/AmirMostofaa/carserv-client",
-    //     live: "https://carserv-76.web.app/",
-    //     img: "https://i.ibb.co/9vnFr31/1.jpg",
-    //     tech1: "React",
-    //     tech2: "Express JS",
-    //     tech3: "MongoDB",
-    //     tech4: "Firebase, Heroku"
-    // },
     {
         id: 2,
         name: "Ogani | Grocery Shop",
-        description: "An almost Full-stack online shop where We can make orders, check the order List.also, we can add Products on Site and Manage the products.",
+        description: "An MERN-Stack online Grocery shop where We can make orders, check the order List.also, we can add Products on Site and Manage the products. Customer can check their previous orders",
         github: "https://github.com/AmirMostofaa/ogani-client",
         live: "https://ogani-d469b.web.app/",
         img: "https://i.ibb.co/Dw38w8v/ogani.png",
@@ -36,12 +24,24 @@ const projects = [
         tech4: "Firebase, Heroku"
     },
     {
+        id: 11,
+        name: "Netflix clone",
+        description: "Netflix clone using React JS, React Movie Trailer, React Youtube with Fully Mobile Responsive. Also instantly we can watch the available movie trailer",
+        github: "https://github.com/AmirMostofaa/netflix-clone",
+        live: "https://netflix-clone-by-amir.web.app/",
+        img: "https://i.ibb.co/wsgvMHk/netflix-min.jpg",
+        tech1: "React",
+        tech2: "React Youtube",
+        tech3: "React Movie Trailer",
+        tech4: "Firebase"
+    },
+    {
         id: 3,
         name: "Wow Rider",
         description: "A riding app, where you can choose a vehicle white there price with Google Auth",
         github: "https://github.com/AmirMostofaa/wow-rider",
         live: "https://wow-rider-bc72a.web.app/",
-        img: "https://i.ibb.co/y0kkqKr/wow.png",
+        // img: "https://i.ibb.co/y0kkqKr/wow.png",
         tech1: "React",
         tech2: "React-Bootstrap",
         tech3: "Firebase",
@@ -59,18 +59,18 @@ const projects = [
         tech3: "Netlify",
         tech4: "Private Route"
     },
-    {
-        id: 5,
-        name: "Flight Booking",
-        description: "A flight Booking Cart of Business Class and Economy Class with different prices",
-        github: "https://github.com/AmirMostofaa/flight-booking",
-        live: "https://amirmostofaa.github.io/flight-booking/",
-        // img: "https://i.ibb.co/R0wwLYD/5.jpg",
-        tech1: "Vanilla JS",
-        tech2: "Bootstrap",
-        tech3: "Github page",
-        tech4: "JS DOM"
-    },
+    // {
+    //     id: 5,
+    //     name: "Flight Booking",
+    //     description: "A flight Booking Cart of Business Class and Economy Class with different prices",
+    //     github: "https://github.com/AmirMostofaa/flight-booking",
+    //     live: "https://amirmostofaa.github.io/flight-booking/",
+    //     // img: "https://i.ibb.co/R0wwLYD/5.jpg",
+    //     tech1: "Vanilla JS",
+    //     tech2: "Bootstrap",
+    //     tech3: "Github page",
+    //     tech4: "JS DOM"
+    // },
     {
         id: 6,
         name: "Team Building",
@@ -83,41 +83,41 @@ const projects = [
         tech3: "Netlify",
         tech4: "Font awesome"
     },
-    {
-        id: 7,
-        name: "Team Tracker",
-        description: "An app of all Teams, you can check about the Team",
-        github: "https://github.com/AmirMostofaa/team-tracker",
-        live: "https://team-tracker-76.netlify.app/",
-        // img: "https://i.ibb.co/bQr9YB0/track.png",
-        tech1: "React",
-        tech2: "React-Bootstrap",
-        tech3: "Netlify",
-        tech4: "API"
-    },
-    {
-        id: 8,
-        name: "Payoneer Bank",
-        description: "A Bank Account Dashboard to Deposit money and withdraw money.",
-        github: "https://github.com/AmirMostofaa/Pioneer",
-        live: "https://amirmostofaa.github.io/Pioneer/",
-        // img: "https://i.ibb.co/yR8Zjzz/payo.png",
-        tech1: "Vanilla JS",
-        tech2: "Bootstrap",
-        tech3: "Github Page",
-        tech4: "."
-    },
-    {
-        id: 9,
-        name: "Calculator",
-        description: "Vanilla JS Calculator",
-        github: "https://github.com/AmirMostofaa/javascript-calculator",
-        live: "https://amirmostofaa.github.io/javascript-calculator/",
-        // img: "https://i.ibb.co/vhNDKf5/calc.png",
-        tech1: "Vanilla JS",
-        tech2: "Bootstrap",
-        tech3: "Github Page",
-        tech4: "."
-    },
+    // {
+    //     id: 7,
+    //     name: "Team Tracker",
+    //     description: "An app of all Teams, you can check about the Team",
+    //     github: "https://github.com/AmirMostofaa/team-tracker",
+    //     live: "https://team-tracker-76.netlify.app/",
+    //     // img: "https://i.ibb.co/bQr9YB0/track.png",
+    //     tech1: "React",
+    //     tech2: "React-Bootstrap",
+    //     tech3: "Netlify",
+    //     tech4: "API"
+    // },
+    // {
+    //     id: 8,
+    //     name: "Payoneer Bank",
+    //     description: "A Bank Account Dashboard to Deposit money and withdraw money.",
+    //     github: "https://github.com/AmirMostofaa/Pioneer",
+    //     live: "https://amirmostofaa.github.io/Pioneer/",
+    //     // img: "https://i.ibb.co/yR8Zjzz/payo.png",
+    //     tech1: "Vanilla JS",
+    //     tech2: "Bootstrap",
+    //     tech3: "Github Page",
+    //     tech4: "."
+    // },
+    // {
+    //     id: 9,
+    //     name: "Calculator",
+    //     description: "Vanilla JS Calculator",
+    //     github: "https://github.com/AmirMostofaa/javascript-calculator",
+    //     live: "https://amirmostofaa.github.io/javascript-calculator/",
+    //     // img: "https://i.ibb.co/vhNDKf5/calc.png",
+    //     tech1: "Vanilla JS",
+    //     tech2: "Bootstrap",
+    //     tech3: "Github Page",
+    //     tech4: "."
+    // },
 ]
 export default projects;
